c0242b89f549681b15246696d6a501be27ce2272
chanLee <2535849195@qq.com>
2025-07-10 11:45:09 +0800

    feat: add dark mode support

---
+++ frontend/src/components/ui/theme-switcher/ThemeSwitcher.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useDarkMode } from '@/hooks/useDarkMode'
+import { Button } from '@/components/ui/button'
+import { Moon, Sun } from 'lucide-vue-next'
+import { type HTMLAttributes } from 'vue'
+
+const { toggleMode, isLight } = useDarkMode()
+
+interface Props {
+  containerClass?: HTMLAttributes['class']
+  class?: HTMLAttributes['class']
+}
+
+const props = defineProps<Props>()
+</script>
+<template>
+  <Button
+    :class="props.containerClass"
+    size="icon"
+    variant="ghost"
+    @click="toggleMode"
+  >
+    <Moon v-if="isLight" :class="props.class" />
+    <Sun v-else :class="props.class" />
+  </Button>
+</template>
